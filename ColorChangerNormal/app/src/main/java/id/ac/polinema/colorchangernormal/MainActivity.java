a80bfaa003bd19b2171bd80deb0ed3e93cd8958d
sofiull <sofiulfr@gmail.com>
2019-11-05 22:23:12 +0700

    Menyelesaikan Viewmodel sederhana percobaan 2

--- ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
+++ ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
@@ -2,6 +2,9 @@ package id.ac.polinema.colorchangernormal;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelStoreOwner;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -14,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
 	private ConstraintLayout rootView;
 	private Button btnChangeColor;
+	// Tambahkan ViewModel
+	ColorViewModel colorViewModel;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +26,15 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		rootView = findViewById(R.id.layout_main);
 		btnChangeColor = findViewById(R.id.change_color);
-		
-		rootView.setBackgroundColor(generateRandomColor());
+		colorViewModel = ViewModelProviders.of(this).get(ColorViewModel.class);
+		rootView.setBackgroundColor(colorViewModel.getColor());
 
 		// Tambahkan event klik pada tombol
 		btnChangeColor.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view){
 				int color = generateRandomColor();
-				rootView.setBackgroundColor(color);
+				rootView.setBackgroundColor(colorViewModel.getColor());
+				colorViewModel.setColor(color);
 			}
 		});
 	}
